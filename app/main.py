65d22011ec479e5d93a896e75970c3491b624b73
Gerson Gutierrez <gersongutierrez64@gmail.com>
2024-03-26 20:36:34 -0600

    2nd test

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# Uncomment this to pass the first stage
 import socket
 
 
@@ -6,11 +5,14 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    conn, addr = server_socket.accept() # wait for client
+    
+    with conn:
+        print("Connected to:", addr)
+        data = conn.recv(1024)
+        conn.sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
+    server_socket.close()
 
 if __name__ == "__main__":
     main()

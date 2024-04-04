9e4e45b79c7f7c3efa62340fbe999999652def08
Gerson Gutierrez <gersongutierrez64@gmail.com>
2024-04-03 19:18:00 -0600

    6th stage

--- app/main.py
+++ app/main.py
@@ -1,43 +1,50 @@
 import socket
-from .http_utils import HttpRequest
+from http_utils import HttpRequest
+import threading
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
     
-    with conn:
-        print(f"Connected to: {addr}")
-        data = conn.recv(1024).decode()
-        print(f"Receive data: {data}")
-        path = data.split()[1]
-        path_elements = path.split("/")
-        path_type = path_elements[1]
-        
-        #Parsing string to object
-        http_request = HttpRequest(data)
-        
-        match path_type:
-            case "echo":
-                msg = ""
-                for i in range(2, len(path_elements)):
-                    msg += path_elements[i]
-                    if i < len(path_elements) - 1:
-                        msg += "/"
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(msg)}\r\n\r\n{msg}"
-                conn.sendall(response.encode())
-            case "user-agent":
-                msg = http_request.headers[path_type]
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(msg)}\r\n\r\n{msg}"
-                conn.sendall(response.encode())
-            case _:
-                if path == "/":
-                    conn.sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
-                else:
-                    conn.sendall("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-    server_socket.close()
+    try:
+        while True:
+            conn, addr = server_socket.accept() # wait for client
+            print(f"Connected to: {addr}")
+            client_thread = threading.Thread(target=process_client, args=[conn])
+            client_thread.start()
+    finally:
+        server_socket.close()
+
+def process_client(conn):
+    data = conn.recv(1024).decode()
+    print(f"Receive data: {data}")
+    path = data.split()[1]
+    path_elements = path.split("/")
+    path_type = path_elements[1]
+    
+    #Parsing string to object
+    http_request = HttpRequest(data)
+    
+    match path_type:
+        case "echo":
+            msg = ""
+            for i in range(2, len(path_elements)):
+                msg += path_elements[i]
+                if i < len(path_elements) - 1:
+                    msg += "/"
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(msg)}\r\n\r\n{msg}"
+            conn.sendall(response.encode())
+        case "user-agent":
+            msg = http_request.headers[path_type]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(msg)}\r\n\r\n{msg}"
+            conn.sendall(response.encode())
+        case _:
+            if path == "/":
+                conn.sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
+            else:
+                conn.sendall("HTTP/1.1 404 Not Found\r\n\r\n".encode())
 
 if __name__ == "__main__":
     main()
